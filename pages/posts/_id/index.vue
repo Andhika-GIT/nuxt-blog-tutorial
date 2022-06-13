fe2b69540d0b70363e7bec42226e0fde1e88f5a3
Andhika Prasetya <andhikapr081@gmail.com>
2022-06-13 11:28:36 +0700

    adding async to single post

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -3,27 +3,47 @@
     <div class="single-post-page">
       <section class="post">
         <h1 class="post-title">
-          Title of the post with id : {{ $route.params.id }}
+          {{ loadedPost.title }}
         </h1>
         <div class="post-details">
-          <div class="post-detail">Last updated on XXX</div>
-          <div class="post-detail">Written by NAME</div>
+          <div class="post-detail">Last updated on {{ loadedPost.updateData }}</div>
+          <div class="post-detail">Written by {{ loadedPost.author }}</div>
         </div>
-        <p>content of the post</p>
+        <p>{{ loadedPost.content }}</p>
       </section>
       <section class="post-feedback">
         <p class="post-content">
           let me know what you think about the post, send a mail to
-          <a
-            target="”_blank”"
-            href="https://www.facebook.com/photo/?fbid=1430222807015576&set=a.597011410336724"
-            >Andhika</a
-          >
+          <a target="”_blank”"
+            href="https://www.facebook.com/photo/?fbid=1430222807015576&set=a.597011410336724">Andhika</a>
         </p>
       </section>
     </div>
   </div>
 </template>
+<script>
+export default {
+  asyncData(context, callback) {
+    console.log(context)
+    setTimeout(() => {
+      callback(null, {
+        loadedPost: {
+          id: `${context.params.id}`, // take the id from route url,
+          title: `First Post (ID : ${context.params.id})`,
+          previewText: "this is our first post",
+          author: "Hubla",
+          updateData: new Date(),
+          content: "some text for content of this post okay",
+          thumbnail:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2lceG8UoY7MwfJS7xXLn-S70J7yUNqcd0d5eVazbGXmIwMfNqF3iO96UXDN3DRIKhGCk&usqp=CAU",
+
+        }
+      })
+    }, 1000)
+  }
+}
+
+</script>
 <style scoped>
 .single-post-page {
   padding: 30px;
