2029224c52582409d1e6deaed165054232362e54
Andhika Prasetya <andhikapr081@gmail.com>
2022-06-13 12:16:13 +0700

    using new promise for async data for posts

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -9,27 +9,33 @@
 import PostList from "~/components/Posts/PostList.vue";
 export default {
   components: { PostList },
-  asyncData(context, callback) {
-    setTimeout(() => {
-      callback(null, {
-        loadedPosts: [
-          {
-            id: "1",
-            title: "first post",
-            previewText: "this is our first post",
-            thumbnail:
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2lceG8UoY7MwfJS7xXLn-S70J7yUNqcd0d5eVazbGXmIwMfNqF3iO96UXDN3DRIKhGCk&usqp=CAU",
-          },
-          {
-            id: "2",
-            title: "second post",
-            previewText: "this is our second post",
-            thumbnail:
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2lceG8UoY7MwfJS7xXLn-S70J7yUNqcd0d5eVazbGXmIwMfNqF3iO96UXDN3DRIKhGCk&usqp=CAU",
-          },
-        ],
-      });
-    }, 1500);
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          loadedPosts: [
+            {
+              id: "1",
+              title: "first post",
+              previewText: "this is our first post",
+              thumbnail:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2lceG8UoY7MwfJS7xXLn-S70J7yUNqcd0d5eVazbGXmIwMfNqF3iO96UXDN3DRIKhGCk&usqp=CAU",
+            },
+            {
+              id: "2",
+              title: "second post",
+              previewText: "this is our second post",
+              thumbnail:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2lceG8UoY7MwfJS7xXLn-S70J7yUNqcd0d5eVazbGXmIwMfNqF3iO96UXDN3DRIKhGCk&usqp=CAU",
+            },
+          ],
+        });
+      }, 1500);
+      // reject(new Error())
+    }).then(data => {
+      return data
+    }).catch(e => { context.error(e) })
+
   },
 };
 </script>
