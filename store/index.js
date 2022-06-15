c1b8477fab5673efe3ea0719b0ec8ace356cf258
Andhika Prasetya <andhikapr081@gmail.com>
2022-06-15 16:57:59 +0700

    using nuxtServerInit for fetching in vuex store

--- store/index.js
+++ store/index.js
@@ -13,10 +13,36 @@ const createStore = () => {
             }
         },
         actions : {
-            // actions to run or commit the method in mutations
-            setPosts(vuexContent, posts) {
-                vuexContent.commit('setPosts', posts) // posts will second parameter for setPosts in mutations
-            }
+            // nuxtServerInit will automatically dispatched this action
+            // this will also automatically check if there's a data (data > 0) after we fetched
+            nuxtServerInit(vuexContent,context) {
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                      vuexContent.commit('setPosts',  [
+                        // fetch the data from store will make the blog posts page faster 
+                        // if there's no changes in data after we fetch, then we only fetch one time which will make the blog posts page faster
+                          {
+                            id: "1",
+                            title: "first post",
+                            previewText: "this is our first post",
+                            thumbnail:
+                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2lceG8UoY7MwfJS7xXLn-S70J7yUNqcd0d5eVazbGXmIwMfNqF3iO96UXDN3DRIKhGCk&usqp=CAU",
+                          },
+                          {
+                            id: "2",
+                            title: "second post",
+                            previewText: "this is our second post",
+                            thumbnail:
+                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2lceG8UoY7MwfJS7xXLn-S70J7yUNqcd0d5eVazbGXmIwMfNqF3iO96UXDN3DRIKhGCk&usqp=CAU",
+                          },
+                        ],
+                      )
+                      resolve();
+                    }, 1500);
+                    // reject(new Error())
+                  })
+            },
+           
         },
         getters : {
             // to get or return the data
