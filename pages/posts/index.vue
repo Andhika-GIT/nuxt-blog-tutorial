8c7b6a8b634f9bee82722d6f24777ef2fd88fb17
Andhika Prasetya <andhikapr081@gmail.com>
2022-06-15 17:02:03 +0700

    use computed to take data getters from vuex store in post index

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -9,42 +9,12 @@
 import PostList from "~/components/Posts/PostList.vue";
 export default {
   components: { PostList },
-  fetch(context) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          loadedPosts: [
-            {
-              id: "1",
-              title: "first post",
-              previewText: "this is our first post",
-              thumbnail:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2lceG8UoY7MwfJS7xXLn-S70J7yUNqcd0d5eVazbGXmIwMfNqF3iO96UXDN3DRIKhGCk&usqp=CAU",
-            },
-            {
-              id: "2",
-              title: "second post",
-              previewText: "this is our second post",
-              thumbnail:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2lceG8UoY7MwfJS7xXLn-S70J7yUNqcd0d5eVazbGXmIwMfNqF3iO96UXDN3DRIKhGCk&usqp=CAU",
-            },
-          ],
-        });
-      }, 1500);
-      // reject(new Error())
-    }).then(data => {
-      context.store.commit('setPosts', data.loadedPosts) // run the mutation from vuex store
-    }).catch(e => { context.error(e) })
-
-  },
   computed: {
     loadedPosts() {
-      return this.$store.getters.loadedPosts // get the data from store so we can use it on view
+      return this.$store.getters.loadedPosts // get the data from vuex store so we can use it on view
     }
   }
-  // created() {
-  //   this.$store.dispatch('setPosts', this.loadedPosts)
-  // }
+
 
 };
 </script>
