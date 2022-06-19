8d4f05c4d599417b83b0e474be007827d9fd909c
Andhika Prasetya <andhikapr081@gmail.com>
2022-06-19 15:17:22 +0700

    send request post data to firebase

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -2,15 +2,29 @@
   <div>
     <div class="admin-new-post-page">
       <section class="new-post-form">
-        <AdminPostForm />
+        <AdminPostForm @submit="onSubmitted" />
       </section>
     </div>
   </div>
 </template>
 <script>
 import AdminPostForm from "~/components/Admin/AdminPostForm.vue";
+import axios from "axios";
 export default {
   components: { AdminPostForm },
+  setup() {
+    // postData is editedPost that came from custom event in AdminPostForm.vue
+    const onSubmitted = (postData) => {
+      // console.log(postData)
+      // send request post to save data into firebase
+      axios.post('https://nuxt-blog-755f4-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json', postData)
+        .then(result => { console.log(result) })
+        .catch(e => console.log(e))
+
+    }
+
+    return { onSubmitted }
+  }
 };
 </script>
 
@@ -25,4 +39,4 @@ export default {
     width: 500px;
   }
 }
-</style>
+</style> 
