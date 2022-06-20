932cb6c3deb8859ce8404f77114b562a281e8b1c
Andhika Prasetya <andhikapr081@gmail.com>
2022-06-20 17:17:43 +0700

    run 'editpost' action after submit edit post

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -9,7 +9,7 @@
 </template>
 <script>
 import AdminPostForm from "~/components/Admin/AdminPostForm.vue";
-import { useRoute, useRouter } from "@nuxtjs/composition-api";
+import { useRoute, useRouter, useStore } from "@nuxtjs/composition-api";
 import axios from "axios";
 export default {
   components: { AdminPostForm },
@@ -25,7 +25,8 @@ export default {
       .then((res) => {
         console.log(res); // check the respond ( look for the data post )
         return {
-          loadedPost: res.data,
+          loadedPost: { ...res.data, id: context.params.postId },
+          // we need retrive id, so we can send it later as a argument when we perform dispatch action after the admin submit the edit post form
         };
       })
       .catch((e) => context.error(e));
@@ -35,15 +36,11 @@ export default {
   setup() {
     const route = useRoute();
     const router = useRouter();
+    const store = useStore();
     const onSubmitted = (editedPost) => {
-      // send put request (update)
-      axios
-        .put(
-          `https://nuxt-blog-755f4-default-rtdb.asia-southeast1.firebasedatabase.app/posts/${route.value.params.postId}.json`,
-          editedPost
-        ) // send the editedPost data from AdminPostForm (emit custom event) to firebase
-        .then((res) => router.push("/admin")) // go back to admin index
-        .catch((e) => console.log(e));
+      // run the 'editPost' action in index vuex store and send editedPost as an argument
+      // editedPost data from AdminPostForm (emit custom event) to 'editPost' action
+      store.dispatch("editPost", editedPost).then(() => router.push("/admin")); // go back to admin index
     };
 
     return { onSubmitted };
