80a8156463447d407fc2ddc0e14a742f5752a500
Andhika Prasetya <andhikapr081@gmail.com>
2022-06-20 17:14:54 +0700

    adding addPost and editPost for mutation and actions

--- store/index.js
+++ store/index.js
@@ -1,44 +1,96 @@
-import Vuex from 'vuex'
-import axios from 'axios'
+import Vuex from "vuex";
+import axios from "axios";
+import { useRoute, useRouter } from "@nuxtjs/composition-api";
 
 const createStore = () => {
-    return new Vuex.Store({
-        // set default variabel for data
-        state : {
-            loadedPosts: []
-        },
-        mutations : {
-            // method for what you want to do with the data
-            setPosts(state,posts) {
-                state.loadedPosts = posts
+  return new Vuex.Store({
+    // set default variabel for data
+    state: {
+      loadedPosts: [], // this will containts all the post data after nuxtServerInit action runs when the website is open
+    },
+    mutations: {
+      // method for what you want to do with the data
+      setPosts(state, posts) {
+        state.loadedPosts = posts;
+      },
+      addPost(state, post) {
+        // 'post' argument is retrive from addPost action
+        // push new index into loadedPost
+        state.loadedPosts.push(post);
+        // so after the redirect, we will the new post that admin have made
+      },
+      editPost(state, editedPost) {
+        // find the post id from database that matched with the id post that has edited, store it in postIndex variabel
+        const postIndex = state.loadedPosts.findIndex(
+          (post) => post.id === editedPost.id
+        );
+        // find post with the index of the post that has been edited, then replace the previous post index
+        // this will replace the previous post that's not updated with the new updated post
+        state.loadedPosts[postIndex] = editedPost;
+      },
+    },
+    actions: {
+      // nuxtServerInit will automatically dispatched this action
+      nuxtServerInit(vuexContent, context) {
+        // nuxtServerInit will only run fetch on initial load (when website is open for the first time), which will make the blog posts page faster
+        return axios
+          .get(
+            "https://nuxt-blog-755f4-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json"
+          )
+          .then((res) => {
+            // console.log(res)
+            const postsArray = []; // make variable to store the data
+            for (const key in res.data) {
+              // for every data array inside the res.data
+              postsArray.push({ ...res.data[key], id: key }); // store the data using push array into postsArray variable
             }
-        },
-        actions : {
-            // nuxtServerInit will automatically dispatched this action
-            // this will also automatically check if there's a data (data > 0) after we fetched
-            nuxtServerInit(vuexContent,context) {
-               // if there's no changes in data after we send request, then we run this request only once, which will make the blog posts page faster
-               return axios.get('https://nuxt-blog-755f4-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json')
-               .then(res => {
-                // console.log(res)
-                const postsArray = [] // make variable to store the data
-                for (const key in res.data) { // for every data array inside the res.data
-                  postsArray.push({...res.data[key], id: key}) // store the data using push array into postsArray variable
-                }
-                vuexContent.commit('setPosts',postsArray)}) // run the mutation, with postsArray variable as an argument to store the data 
-               .catch(e => context.error(e))
-               
-            },
-           
-        },
-        getters : {
-            // to get or return the data
-            loadedPosts(state){
-               return state.loadedPosts
-            }
-        }
-    })
+            vuexContent.commit("setPosts", postsArray);
+          }) // run the mutation, with postsArray variable as an argument to store the data
+          .catch((e) => context.error(e));
+      },
+      // action after we submit new post
+      addPost(vuexContent, postData) {
+        // create new variabel for storing new data
+        const createdPost = {
+          // all the new data form submitted form, and also retrive the updatedDate (date when data is submitted)
+          ...postData,
+          updatedDate: new Date(),
+        };
+        // send request post to save data into firebase
+        return axios
+          .post(
+            "https://nuxt-blog-755f4-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json",
+            createdPost
+          ) // send all the new data form submitted form into firebase
+          .then((result) => {
+            // commit the addPost mutation
+            vuexContent.commit("addPost", {
+              ...createdPost,
+              id: result.data.name, // the id name is a unique name created automatically by firebase and we can get that id name after the post is created
+            });
+          }) // go back to admin index
+          .catch((e) => console.log(e));
+      },
+      editPost(vuexContent, editedPost) {
+        return axios
+          .put(
+            `https://nuxt-blog-755f4-default-rtdb.asia-southeast1.firebasedatabase.app/posts/${editedPost.id}.json`,
+            editedPost
+          ) // send the edited post data to firebase by using put request method
+          .then((res) => {
+            // commit the ediPost mutation
+            vuexContent.commit("editPost", editedPost);
+          }) // go back to admin index
+          .catch((e) => console.log(e));
+      },
+    },
+    getters: {
+      // to get or return the data
+      loadedPosts(state) {
+        return state.loadedPosts;
+      },
+    },
+  });
+};
 
-}
-
-export default createStore
+export default createStore;
