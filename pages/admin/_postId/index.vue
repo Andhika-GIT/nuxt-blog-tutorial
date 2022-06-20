1e68821821b2dfd325c50824d20c77ec23f5baab
Andhika Prasetya <andhikapr081@gmail.com>
2022-06-20 15:53:01 +0700

    send put request to update in postId index, after admin submitted update post form

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -2,13 +2,14 @@
   <div>
     <div class="admin-post-page">
       <section class="update-form">
-        <AdminPostForm :post="loadedPost" />
+        <AdminPostForm :post="loadedPost" @submit="onSubmitted" />
       </section>
     </div>
   </div>
 </template>
 <script>
 import AdminPostForm from "~/components/Admin/AdminPostForm.vue";
+import { useRoute, useRouter } from "@nuxtjs/composition-api";
 import axios from "axios";
 export default {
   components: { AdminPostForm },
@@ -16,16 +17,37 @@ export default {
     // console.log(context) -> check what context can do(it can catch the id of the post)
     // send http request to get single post
     // catch the id post using context.params
-    return axios.get(`https://nuxt-blog-755f4-default-rtdb.asia-southeast1.firebasedatabase.app/posts/${context.params.postId}.json`)
-      .then(res => {
-        console.log(res) // check the respond ( look for the data post )
-        return {
-          loadedPost: res.data
-        }
+    return axios
 
+      .get(
+        `https://nuxt-blog-755f4-default-rtdb.asia-southeast1.firebasedatabase.app/posts/${context.params.postId}.json`
+      ) // send get request for single (by sending the id)
+      .then((res) => {
+        console.log(res); // check the respond ( look for the data post )
+        return {
+          loadedPost: res.data,
+        };
       })
-      .catch(e => context.error(e))
-  }
+      .catch((e) => context.error(e));
+  },
+
+  // setup for update
+  setup() {
+    const route = useRoute();
+    const router = useRouter();
+    const onSubmitted = (editedPost) => {
+      // send put request (update)
+      axios
+        .put(
+          `https://nuxt-blog-755f4-default-rtdb.asia-southeast1.firebasedatabase.app/posts/${route.value.params.postId}.json`,
+          editedPost
+        ) // send the editedPost data from AdminPostForm (emit custom event) to firebase
+        .then((res) => router.push("/admin")) // go back to admin index
+        .catch((e) => console.log(e));
+    };
+
+    return { onSubmitted };
+  },
 };
 </script>
 <style scoped>
