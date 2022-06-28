67dae0eefe81347dbe83fffcd7c73bed8ab02f89
Andhika Prasetya <andhikapr081@gmail.com>
2022-06-28 20:56:17 +0700

    adding some console.log to check the status

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,5 @@
 export default function ({ store, redirect }) {
+  console.log('[middleware] action store run on the server, this is run when user signed in')
   if (!store.getters.isAuthenticated) {
     return redirect("/admin/auth");
   }
