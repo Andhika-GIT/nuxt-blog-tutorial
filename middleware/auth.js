14be351944844a2c53ea2d47220edd5e1fd61f7f
Andhika Prasetya <andhikapr081@gmail.com>
2022-07-03 13:51:27 +0700

    fix auth.js fix the store.getters.isAuthenticated

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,7 @@
 export default function ({ store, redirect }) {
-  console.log('[middleware] action store run on the server, this is run when user signed in')
+  console.log(
+    "[middleware] action store run on the server, this is run when user signed in"
+  );
   if (!store.getters.isAuthenticated) {
     return redirect("/admin/auth");
   }
