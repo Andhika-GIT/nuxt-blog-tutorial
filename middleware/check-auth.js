df4d32502fc96c225d40df465fb76f4ce9e6a9a4
Andhika Prasetya <andhikapr081@gmail.com>
2022-07-04 15:23:41 +0700

    use context.req in check-auth.js send context.req parameter, so initAuth action catch it and get the data from cookie instead

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,9 +1,11 @@
-export default function ({ store }) {
-    // if the process is run on the client side ( action store run on the client side, after the user go to admin page for the second time after LOG IN)
-    if (process.client) {
-        // run the 'initAuth' action from vuex store
-        store.dispatch('initAuth')
-        // console.log for checking
-        console.log('[middleware] user go to admin for the second time, now the action is excuted on client, and token is successfuly stored in the localstorage')
-    }
+export default function (context) {
+  // if the process is run on the client side ( action store run on the client side, after the user go to admin page for the second time after LOG IN)
+
+  // run the 'initAuth' action from vuex store
+  context.store.dispatch("initAuth", context.req);
+  // send context.req parameter, so initAuth action catch it and get the data from cookie instead
+  // console.log for checking
+  console.log(
+    "[middleware] user go to admin for the second time, now the action is excuted on client, and token is successfuly stored in the localstorage"
+  );
 }
