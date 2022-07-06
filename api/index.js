3f01e56bb9b557f8c44fcf3b8bb0bc4a87154c8c
Andhika Prasetya <andhikapr081@gmail.com>
2022-07-06 10:36:52 +0700

    server side code test

--- api/index.js
+++ api/index.js
@@ -4,4 +4,6 @@ const router = express.Router();
 
 router.post("/track-data", (req, res) => {
   // write any serverside code
+  console.log("Stored Data!", req.body.data); // write data result in req.body.data
+  res.status(200).json({ message: "Success !" }); // send status respond and message
 });
