f064ba17aefa7de530a17bbe3b5129e936295c2a
Andhika Prasetya <andhikapr081@gmail.com>
2022-07-06 10:53:11 +0700

    express js code setup for req and res

--- api/index.js
+++ api/index.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
+// extension of the express app, code setup for incoming request and respond
+const app = express();
+router.use((req, res, next) => {
+  Object.setPrototypeOf(req, app.request);
+  Object.setPrototypeOf(res, app.response);
+  req.res = res;
+  res.req = req;
+  next();
+});
+
 router.post("/track-data", (req, res) => {
   // write any serverside code
   console.log("Stored Data!", req.body.data); // write data result in req.body.data
